d1e383145c913754f4cbd4adc35adfc25e6ebd74
Your Name <you@example.com>
2026-01-20 18:21:45 +0000

    mes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,59 @@
 <script setup lang="ts">
+import type { PropType } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+import { type RouteRecordRaw } from 'vue-router'
+/*
+type routeProp = {
+  routes: RouteRecordRaw[]
+}
+const props = defineProps<routeProp>();
+*/
 </script>
 
+<script lang="ts">
+  
+  export default {
+    props: {
+      routes: {
+         type: Array, //Array as PropType<RouteRecordRaw[]>,
+        
+         default: () => []
+      }
+    },
+    methods: {
+      alrt(){
+        alert(this.routes);  
+      }
+    },
+    setup(props){
+      alert(props.routes?);
+    },
+  }
+</script>
 <template>
-  <div>
+<!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <router-link to="/default1">default1</router-link>
-    <router-link to="/default2">default2</router-link>
-    <router-link to="/default3">default3</router-link>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+</div> -->
+<div>
+<button target="_blank" @click="alrt">alert</button>
+  <router-link v-for="route in routes" :key="route.path" :to="route.path">
+    {{route.name}}
+  </router-link>
+
+<span>wtf</span>
+</div>
+<!--    <router-link to="/default1">default1</router-link>
+    <router-link to="/default2">defaut2</router-link>
+    <router-link to="/default3">default3</router-link> -->
+ 
+<!--  <HelloWorld msg="Vite + Vue" /> -->
   <router-view></router-view>
+
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,53 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 // route components
 import default1 from './components/default1.vue'
 import default2 from './components/default2.vue'
 import default3 from './components/default3.vue'
 
+type routeProp = {
+  routes: RouteRecordRaw[]
+}
+const routeRecords: RouteRecordRaw[] = [
+  {
+    path: '/default1',
+    component: default1,
+    name: 'home',
+    meta: {
+      menu: [
+        'defaultMenu'
+      ],
+    }
+  },
+
+  {
+    path: '/default2',
+    name: 'page2',
+    component: default2,
+    meta: {
+      menu: [
+        'defaultMenu'
+      ],
+    }
+  },
+  { path: '/default3',
+     name: 'page3',
+    component: default3,
+    meta: {
+      menu: [
+        'defaultMenu'
+      ],
+    }
+  } ,
+];
+
+
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/default1', component: default1},
-    
-    { path: '/default2', component: default2},
-    { path: '/default3', component: default3} ,
-   ]
+  routes: routeRecords,
 })
 const app = createApp(App)
 app.use(router);
-app.mount('#app')
+app.mount('#app', { routes: routeRecords });
